1f442635dcffee0d243c7494ba2c3289ccde54c9
Mundra-1997 <115582956+Mundra-1997@users.noreply.github.com>
2023-04-21 22:38:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ import DBConnection from "./database/db.js";
 const app = express();
 app.use(cors());
 app.use('/',router);
-app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
-  })
+// app.get('/', (_, res) => {
+//     res.status(200).json({ message: 'Welcome to the Support Desk API' })
+//   })
 const PORT = 8000;
 DBConnection();
 app.listen(PORT,()=>{
